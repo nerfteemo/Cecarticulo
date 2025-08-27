5b3fa308b8afc90ef8f58c56f250b16174ac4096
Julio Cesar Martinez <juliocesarmartinez1344@gmail.com>
2025-08-27 12:55:10 -0500

    first commit

---
+++ src/main/java/com/aej/cecarticulo/CecarticuloApplication.java
@@ -0,0 +1,13 @@
+package com.aej.cecarticulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CecarticuloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CecarticuloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aej/cecarticulo/CecarticuloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aej.cecarticulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CecarticuloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
